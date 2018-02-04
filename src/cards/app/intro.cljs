f1d63b01417968cc35369b5a794dcbd1cf908be5
djjolicoeur <djjolicoeur@gmail.com>
2018-02-03 20:19:06 -0500

    Chap 2.92

--- src/cards/app/intro.cljs
+++ src/cards/app/intro.cljs
@@ -1,7 +1,13 @@
 (ns app.intro
-  (:require [devcards.core :as rc :refer-macros [defcard]]
+  (:require [fulcro.client.cards :refer [defcard-fulcro]]
+            [app.ui.root :as root]
             [app.ui.components :as comp]))
 
-(defcard SVGPlaceholder
-  "# SVG Placeholder"
-  (comp/ui-placeholder {:w 200 :h 200}))
+;; (defcard SVGPlaceholder
+;;   "# SVG Placeholder"
+;;   (comp/ui-placeholder {:w 200 :h 200}))
+
+(defcard-fulcro sample-app
+  root/Root
+  {}
+  {:inspect-data true})
